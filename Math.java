750186bbf834e9c34557104058f630faeefe56e2
ayoub abdi <ayoubabdi2019@gmail.com>
2025-10-28 10:12:54 +0000

    Added Math class with min function

--- Math.java
+++ Math.java
@@ -12,3 +12,20 @@
     // Divison
 System.out.println("Divided together; " + divide(num1, num2)); // Done by Manaal Aouttah
 System.out.println("First Number Divided from Second Number; " + divide(num1, num2)); // Done by Manaal Aouttah
+
+
+/********************************
+    Developer: Ayoub Abdisalam
+    University ID: 240240392
+    Function: This function takes two inputs as integers and returns the sum
+    ********************************/
+
+     public static int min(int a, int b) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+          
+     }   
+     
