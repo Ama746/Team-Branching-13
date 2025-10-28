8e57bf4195900eae9e8bf8ad62c2b3b7c85bd470
Dimitri Khair-Cabalan <240124715@aston.ac.uk>
2025-10-28 11:54:45 +0000

    Dim- Multi

--- Math.java
+++ Math.java
@@ -12,3 +12,11 @@
     // Divison
 System.out.println("Divided together; " + divide(num1, num2)); // Done by Manaal Aouttah
 System.out.println("First Number Divided from Second Number; " + divide(num1, num2)); // Done by Manaal Aouttah
+/****************************
+ * Developer: Dimitri Khair-Cabalan
+ * Univeristy ID: 240124715
+ * Function: Multiplication
+ ****************************/
+public static int Multiplication(int a, int b){
+    return a * b;
+}
