21f005878f488904729b53cb3b2b755c5e7cfd96
Amatullaah Stevenson <160158221@aston.ac.uk>
2025-10-28 11:31:30 +0000

    Added math max function to own branch

---
+++ MathFunctions.java
@@ -0,0 +1,19 @@
+    /********************************
+    Developer: Amatullaah Stevenson
+    University ID: 160158221
+    Function: This function takes two inputs as integers and returns the maximum one.
+    ********************************/
+
+     public class MathFunctions
+    public static void main(String[] args) {
+                // Max
+        System.out.println("Maximum integer is: " + max);
+    }
+    
+    public static int max(int a, int b) {
+        if (a > b ) {
+            return a;
+        } else {
+            return b;
+        }
+    }
