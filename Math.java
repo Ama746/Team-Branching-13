9ba9e14697c08a8b1d22703e196338b5557b3fd4
ayoub abdi <ayoubabdi2019@gmail.com>
2025-10-28 11:28:00 +0000

    Updated Math function

--- Math.java
+++ Math.java
@@ -1,23 +1,8 @@
-/********************************
-    Developer: Manaal Aouttah
-    University ID: 240124977
-    Function: Division
-
-    ********************************/
-
-    // Write your function here
-    public static int divide(int a, int b) {
-        return a / b;
-    }
-    // Divison
-System.out.println("Divided together; " + divide(num1, num2)); // Done by Manaal Aouttah
-System.out.println("First Number Divided from Second Number; " + divide(num1, num2)); // Done by Manaal Aouttah
-
 
 /********************************
     Developer: Ayoub Abdisalam
     University ID: 240240392
-    Function: This function takes two inputs as integers and returns the sum
+    Function: This function takes two inputs as integers and returns the min
     ********************************/
 
      public static int min(int a, int b) {
