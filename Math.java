b528c8f74dfd2f17b86bf8629dadee2ccd7f3e6a
Navdeep Malhi <230042537@aston.ac.uk>
2025-10-28 11:17:19 +0000

    Adding math file

---
+++ Math.java
@@ -0,0 +1,19 @@
+/********************************
+    Developer: Navdeep Malhi
+    University ID: 230042537
+    Function: This function takes two inputs as integers and returns the sum
+    ********************************/
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    /********************************
+    Developer: Navdeep Malhi
+    University ID: 230042537
+    Function: This function takes two inputs as integers and subtracts the first number from the second number
+    ********************************/
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
