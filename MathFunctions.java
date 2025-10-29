44c4692de31e9e587e35b2bf07209ea48b302042
Amatullaah Stevenson <160158221@aston.ac.uk>
2025-10-29 11:52:23 +0000

    added main class so code will run correctly

--- MathFunctions.java
+++ MathFunctions.java
@@ -1,4 +1,5 @@
-    /********************************
+   import java.util.Scanner;
+   /********************************
     Developer: Amatullaah Stevenson
     University ID: 160158221
     Function: This function takes two inputs as integers and returns the maximum one.
@@ -6,8 +7,13 @@
 
      public class MathFunctions {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter  first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter  second number: ");
+        int b = sc.nextInt();
                 // Max
-        System.out.println("Maximum integer is: " + max);
+        System.out.println("Maximum integer is: " + max(a,b));
     }
     
     public static int max(int a, int b) {
